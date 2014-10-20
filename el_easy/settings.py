7a3f65fc3e15cf900cc35b61e624b5080468e8c0
Giulliano Morroni <giullianomorroni@gmail.com>
2014-10-20 16:36:00 -0200

    Update settings.py

--- el_easy/settings.py
+++ el_easy/settings.py
@@ -85,16 +85,39 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_URL = '/static/'
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = ''
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = ''
+
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ('css', os.environ['EL_EASY']+'/templates/static/css/'),
-    ('js', os.environ['EL_EASY']+'/templates/static/js/'),
-    ('image', os.environ['EL_EASY_IMG']),
+    ('css', '/var/www/el_easy/el_easy/templates/static/css/'),
+    ('js', '/var/www/el_easy/el_easy/templates/static/js/'),
+    ('image', '/var/www/el_easy/el_easy/templates/static/img/'),
 )
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
-STATIC_URL = '/static/'
