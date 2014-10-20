a9831a38b0d0dc57e21f11015731717e7ee945a5
Giulliano Morroni <giullianomorroni@gmail.com>
2014-10-20 16:28:09 -0200

    Update settings.py

--- el_easy/settings.py
+++ el_easy/settings.py
@@ -89,8 +89,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ('css', os.environ['EL_EASY']+'/el_easy/templates/static/css/'),
-    ('js', os.environ['EL_EASY']+'/el_easy/templates/static/js/'),
+    ('css', os.environ['EL_EASY']+'/templates/static/css/'),
+    ('js', os.environ['EL_EASY']+'/templates/static/js/'),
     ('image', os.environ['EL_EASY_IMG']),
 )
 
