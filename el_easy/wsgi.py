4ddd651b8ff5f46ca1f72e193f618dd8db9add21
Giulliano Morroni <giullianomorroni@gmail.com>
2014-10-20 14:36:39 -0200

    Update wsgi.py

--- el_easy/wsgi.py
+++ el_easy/wsgi.py
@@ -2,13 +2,12 @@ import os
 import sys
 import django.core.handlers.wsgi
 
-#path = '/var/www/casacomigo/new_commerce/new_commerce/'
 #if path not in sys.path:
-sys.path.append('/var/www/el_easy/el_easy')
+sys.path.append('/var/www/el_easy/el_easy/')
 sys.path.append('/var/www/el_easy/')
 
 os.environ['DJANGO_SETTINGS_MODULE'] 	= 'settings'
-os.environ["CASA_COMIGO"] 		= "/var/www/el_easy/el_easy"
-os.environ["CASA_COMIGO_IMG"] 		= "/var/www/static/el_easy"
+os.environ["EL_EASY"] 	             	= "/var/www/el_easy/el_easy"
+os.environ["EL_EASY_IMG"] 		        = "/var/www/static/el_easy"
 
 application = django.core.handlers.wsgi.WSGIHandler()
