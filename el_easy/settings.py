977c774373dcb1c600045c3c4030189548fdc3e9
Giulliano Morroni <giullianomorroni@gmail.com>
2014-10-20 16:48:57 -0200

    Update settings.py

--- el_easy/settings.py
+++ el_easy/settings.py
@@ -108,9 +108,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ('css', '/var/www/el_easy/el_easy/templates/static/css/'),
-    ('js', '/var/www/el_easy/el_easy/templates/static/js/'),
-    ('image', '/var/www/el_easy/el_easy/templates/static/img/'),
 )
 
 # List of finder classes that know how to find static files in
